2ba731c8af9b46f2983b1debef77e30d3091050c
PaperCube <imzhy@hotmail.com>
2021-05-31 12:23:37 +0800

    Try to control sdk permission once more

--- android/src/main/java/com/jiguang/jpush/JPushApp.java
+++ android/src/main/java/com/jiguang/jpush/JPushApp.java
@@ -1,10 +1,21 @@
 package com.jiguang.jpush;
 
-import com.oasisfeng.condom.CondomProcess;
+import android.content.Intent;
+import com.oasisfeng.condom.*;
 import android.app.Application;
 
 public class JPushApp extends Application {
-    @Override public void onCreate() {
-      CondomProcess.installExceptDefaultProcess(this);
+    @Override
+    public void onCreate() {
+        CondomOptions options = new CondomOptions();
+        options.setOutboundJudge(new CustomOutboundJudge());
+        CondomProcess.installExcept(this, options); // install on all
+    }
+
+    public static class CustomOutboundJudge implements OutboundJudge {
+        @Override
+        public boolean shouldAllow(OutboundType type, Intent intent, String targetPackage) {
+            return false;
+        }
     }
 }
